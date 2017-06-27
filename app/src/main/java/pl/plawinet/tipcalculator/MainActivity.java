aba9a96208eaca78a79aae43f33435163affdfdd
Kamil Głusiński <kamil.glusinski@plawinet.pl>
2017-06-27 13:03:42 +0200

    ready commit

--- app/src/main/java/pl/plawinet/tipcalculator/MainActivity.java
+++ app/src/main/java/pl/plawinet/tipcalculator/MainActivity.java
@@ -1,7 +1,10 @@
 package pl.plawinet.tipcalculator;
 
+import java.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -18,9 +21,81 @@ public class MainActivity extends AppCompatActivity {
     private TextView totalLabelTextView;
     private TextView totalTextView;
 
+    //Statyczne obiekty
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    //Kwota rachunku usera i procent napiwku
+    private double billAmount = 0.0;
+    private double tipPercent = 0.15;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicjalizacja elementów GUI
+        amountEditText = (EditText) findViewById(R.id.amountEditText);
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalLabelTextView = (TextView) findViewById(R.id.totalLabelTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+        //Nasluchiwanie zdarzen pola EditText
+        amountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    billAmount = Double.parseDouble(s.toString()) / 100.0;
+                    amountTextView.setText(currencyFormat.format(billAmount));
+                } catch (NumberFormatException ex) {
+                    amountTextView.setText("");
+                    billAmount = 0.0;
+                }
+
+                calculateTipAndTotalAmount();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Nasluchiwanie zdarzen dla pola SeekBar
+        percentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tipPercent = progress / 100.0;
+                calculateTipAndTotalAmount();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void calculateTipAndTotalAmount()
+    {
+        double tipAmount = billAmount * tipPercent;
+        double totalAmount = billAmount + tipAmount;
+
+        percentTextView.setText(percentFormat.format(tipPercent));
+        tipTextView.setText(currencyFormat.format(tipAmount));
+        totalTextView.setText(currencyFormat.format(totalAmount));
     }
 }
